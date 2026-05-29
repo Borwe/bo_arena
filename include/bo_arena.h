bfa83ce888bac98a955fceb779e3fcb15521e55c
brian.orwe <brian.orwe@gmail.com>
2026-05-29 17:03:01 +0300

    fix: bo_arena.h was enabling implemntation by default, comment out.

--- include/bo_arena.h
+++ include/bo_arena.h
@@ -94,7 +94,7 @@ void bo_arena_free(bo_arena *const arena, void *const item){
 }
 
 /** To enable the functions of bo_arena file */
-#define BO_ARENA_IMPLENTATION 
+//#define BO_ARENA_IMPLENTATION 
 #ifdef BO_ARENA_IMPLENTATION
 
 static void *bo_arena__try_get_free_space(
